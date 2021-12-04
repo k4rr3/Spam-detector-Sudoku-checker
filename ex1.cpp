67b31f320f60b0ffbbdf438e4f43857400de8d1f
k4rr3 <88931178+k4rr3@users.noreply.github.com>
2021-12-04 11:09:56 +0100

    Add files via upload
    The program is able to read the spam key words but only stores the last word, so an array of 2D needs to be implemented in order to store all the words as a string format.

--- ex1.cpp
+++ ex1.cpp
@@ -2,51 +2,56 @@
 #include <string.h>
 #include <ctype.h>
 #define N 500
-void jump_spaces(char &space);
-void read_words(char &letter, char searching_words[]);
-void read(char &letter, char searching_words[]);
-void read_first_word(char &letter, char searching_words[]);
+void jump_spaces(char &space, bool &last_word);
+void read_word(char &letter, char searching_words[], int &length, bool &last_word);
+void read(char &letter, char searching_words[], int &length, bool &last_word);
 
 int main()
 {
-    char letter;
-    char searching_words[N + 1];
-    jump_spaces(letter);
-    read_first_word(letter, searching_words);
-    read_words(letter, searching_words);
+    int i = 0, length;
+    char letter, searching_words[N + 1];
+    bool last_word = false;
+    read(letter, searching_words, length, last_word);
+
+    printf("------------------\n");
+    //printf("%i", length);
+    for (i = 0; i < length; i++)
+    {
+        printf("%c", searching_words[i]);
+    }
 }
 
-void read_words(char &letter, char searching_words[])
+void read(char &letter, char searching_words[], int &length, bool &last_word)
 {
-    jump_spaces(letter);
-    read(letter, searching_words);
+    while (last_word == false)
+    {
+        jump_spaces(letter, last_word);
+        read_word(letter, searching_words, length, last_word);
+        if (letter == '.')
+        {
+            last_word = true;
+        }
+    }
 }
-void read(char &letter, char searching_words[])
+void jump_spaces(char &letter, bool &last_word)
+{
+    while (!isalpha(letter))
+    {
+        scanf("%c", &letter);
+    }
+}
+
+void read_word(char &letter, char searching_words[], int &length, bool &last_word)
 {
     int i = 0;
-    scanf("%c", &letter);
-    while (letter != ' ' && letter != '.' && i < N)
+    while (isalpha(letter))
     {
-        // printf("%c",letter);
         searching_words[i] = letter;
         printf("%c", searching_words[i]);
         i++;
         scanf("%c", &letter);
     }
+    length = i;
     searching_words[i] = '\0';
     printf("\n");
-}
-void jump_spaces(char &letter)
-{
-    while (!isalpha(letter))
-    // while (letter == ' ')
-    {
-        scanf("%c", &letter);
-    }
-}
-
-void read_first_word(char &letter, char searching_words[])
-{
-    scanf("%c", &letter);
-    read(letter, searching_words);
 }
