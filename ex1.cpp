bc3ceb771cd89fe8509abb5eceb46e86d18f8dcb
k4rr3 <88931178+k4rr3@users.noreply.github.com>
2021-12-06 20:05:53 +0100

    Add files via upload
    The program is able to save the keywords of the spam checker and read all the text word by word. Those words are temporarily saved in a 2D array until the next word is read.

--- ex1.cpp
+++ ex1.cpp
@@ -6,20 +6,81 @@
 void jump_spaces(char &space, bool &last_word);
 void read_word(char &letter, int &length, bool &last_word, char search_words[][M]);
 void keywords(char &letter, int &length, bool &last_word, char search_words[][M]);
-void search_repeated_words(char &letter, char search_words[][M], int &length, char repeated_words[][M], bool &last_word);
-bool end(char &letter, bool &last_word);
+void search_repeated_words(char &letter, char search_words[][M], char repeated_words[][M], bool &last_word, bool &exit);
+void jump_text_spaces(char &letter, bool &last_word);
+void words_text(char &letter, bool &last_word, char search_words[][M], char repeated_words[][M], bool &exit);
 int main()
 {
     int i = 0, length = 0;
     char search_words[N][M];
     char repeated_words[length][M];
     char letter;
+    bool exit = false;
     bool last_word = false;
+    printf("Introdueix les paraules a identificar:\n");
     keywords(letter, length, last_word, search_words);
     printf("-------------------------\n");
-    search_repeated_words(letter, search_words, length, repeated_words, last_word);
+    printf("Introdueix el text acabat en doble punt:\n");
+    last_word = false;
+    words_text(letter, last_word, search_words, repeated_words, exit);
 }
+void search_repeated_words(char &letter, char search_words[][M], char repeated_words[][M], bool &last_word, bool &exit)
+{
+    int i = 0, j = 1;
+    int coincidences = 0;
+    char temp[M];
+    i = 0;
+    while (isalpha(letter) && last_word == false)
+    {
 
+        letter = tolower(letter);
+        temp[i] = letter;
+        i++;
+        scanf("%c", &letter);
+    }
+    temp[i] = '\0';
+    strcpy(repeated_words[j + 1], temp);
+
+    printf("%s ", repeated_words[j + 1]);
+    j++;
+}
+void jump_text_spaces(char &letter, bool &last_word)
+{
+    while (!isalpha(letter) && last_word == false)
+    {
+        scanf("%c", &letter);
+        if (letter == '.')
+        {
+            scanf("%c", &letter);
+            if (letter == '.')
+            {
+
+                last_word = true;
+            }
+        }
+        else
+        {
+            last_word = false;
+        }
+    }
+}
+void words_text(char &letter, bool &last_word, char search_words[][M], char repeated_words[][M], bool &exit)
+{
+    while (last_word == false)
+    {
+        jump_text_spaces(letter, last_word);
+        search_repeated_words(letter, search_words, repeated_words, last_word, exit);
+        if (letter == '.')
+        {
+            scanf("%c", &letter);
+            // printf("%c", letter);
+            if (letter == '.')
+            {
+                last_word = true;
+            }
+        }
+    }
+}
 void keywords(char &letter, int &length, bool &last_word, char search_words[][M])
 {
     while (last_word == false)
@@ -61,37 +122,4 @@ void read_word(char &letter, int &length, bool &last_word, char search_words[][M
     printf("%s", search_words[length]);
     length++;
     printf("\n");
-}
-bool end(char &letter, bool &last_word)
-{
-    last_word = false;
-    if (letter == '.')
-    {
-        scanf("%c", &letter);
-    }
-    else if (letter == '.')
-    {
-        last_word == true;
-    }
-    return last_word;
-}
-void search_repeated_words(char &letter, char search_words[][M], int &length, char repeated_words[][M], bool &last_word)
-{
-    int i = 0, j = 0;
-    last_word = false;
-    while (last_word == false)
-    {
-        jump_spaces(letter, last_word);
-        scanf("%s", repeated_words[j]);
-        printf("%s", repeated_words[j]);
-        repeated_words[j][M] = tolower(repeated_words[j][M]);
-        for (i = 0; i <= length; i++)
-        {
-            if (search_words[i] == repeated_words[j])
-            {
-                printf("repetida");
-                last_word = true;
-            }
-        }
-    }
 }
